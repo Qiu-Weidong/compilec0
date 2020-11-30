3ac9854880dda867ff4bf69b6ae5600a311a1862
unknown <568385476@qq.com>
2020-11-30 13:58:35 +0800

    .

--- source/main.cpp
+++ source/main.cpp
@@ -6,26 +6,31 @@
 
 int main(int argc, char *argv[])
 {
-    printf("just for test!\n");
     Tokenizer tokenizer;
-    // std::ofstream fout;
-    try
-    {
-        tokenizer.analyse(argv[1]);
-        auto tokens = tokenizer.getTokens();
-        // fout.open(argv[2]);
-        for (const auto &token : tokens)
-        {
-            std::cout << token;
-            // fout << token;
-        }
-    }
-    catch (Error &err)
-    {
-        std::cerr << err << std::endl;
-    }
-    catch (const std::exception &e)
-    {
-        std::cerr << e.what() << '\n';
-    }
+    std::fstream fin;
+    fin.open(argv[2]);
+    std::string buf;
+    std::fstream fout;
+    while(fin >> buf)
+        fout << buf << std::endl;
+    // try
+    // {
+    //     tokenizer.analyse(argv[1]);
+    //     auto tokens = tokenizer.getTokens();
+    //     // fout.open(argv[2]);
+    //     for (const auto &token : tokens)
+    //     {
+    //         std::cout << token;
+    //         // fout << token;
+    //     }
+    // }
+    // catch (Error &err)
+    // {
+    //     std::cerr << err << std::endl;
+    // }
+    // catch (const std::exception &e)
+    // {
+    //     std::cerr << e.what() << '\n';
+    // }
+
 }
