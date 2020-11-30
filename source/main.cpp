aff9b7c52657b172c2cb286175b46953f78b65cc
unknown <568385476@qq.com>
2020-11-30 18:11:24 +0800

    词法分析

--- source/main.cpp
+++ source/main.cpp
@@ -7,32 +7,25 @@
 int main(int argc, char *argv[])
 {
     Tokenizer tokenizer;
-    std::fstream fin;
-    fin.open(argv[2]);
-    std::string buf;
-    while (std::getline(fin,buf))
-    {
-        std::cout << buf << std::endl;
-    }
     
-    // try
-    // {
-    //     tokenizer.analyse(argv[1]);
-    //     auto tokens = tokenizer.getTokens();
-    //     // fout.open(argv[2]);
-    //     for (const auto &token : tokens)
-    //     {
-    //         std::cout << token;
-    //         // fout << token;
-    //     }
-    // }
-    // catch (Error &err)
-    // {
-    //     std::cerr << err << std::endl;
-    // }
-    // catch (const std::exception &e)
-    // {
-    //     std::cerr << e.what() << '\n';
-    // }
+    try
+    {
+        tokenizer.analyse(argv[2]);
+        auto tokens = tokenizer.getTokens();
+        for (const auto &token : tokens)
+        {
+            std::cout << token;
+        }
+    }
+    catch (Error &err)
+    {
+        // std::cerr << err << std::endl;
+        return -1;
+    }
+    catch (const std::exception &e)
+    {
+        // std::cerr << e.what() << '\n';
+        return -1;
+    }
 
 }
